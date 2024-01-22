82f65fd1c6754a626708a048634801007d2036f0
maleahy <maleahyee@gmail.com>
2024-01-22 14:24:54 -0700

    Add project files.

---
+++ Mission2Dice/DiceRoller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Dice
+{
+    class DiceRoller
+    {
+        private Random random = new Random();
+        //// Use an array to keep track of the number of times that each combination is thrown. In other words,
+        //// keep track of how many times the combination of the two simulated dice is 2, how many times
+        //// the combination is 3, and so on, all the way up through 12.
+
+        public int[] RollDice(int numRolls)
+        {
+            int[] total = new int[13];
+
+            for (int rolled = 0; rolled < numRolls; rolled++) // Corrected loop increment
+            {
+                int rollOne = random.Next(1, 7);
+                int rollTwo = random.Next(1, 7);
+                total[rollOne + rollTwo]++;
+            }
+
+            // That method in the second class should return the array containing the results.
+            return total;
+        }
+    }
+}
---
+++ Mission2Dice/DiceWords.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Dice
+
+{
+    class DiceWords
+    {
+        static void Main(string[] args)
+        {
+            // Allow the user to choose how many times the “dice” will be thrown. 
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int numRolls = int.Parse(Console.ReadLine());
+
+            // Then, once you have the number of rolls, pass that number to a second class that has a method that 
+            // simulates the roll of the dice for the number of times that the user specified.
+            // That method in the second class should return the array containing the results. 
+            DiceRoller roller = new DiceRoller();
+            int[] results = roller.RollDice(numRolls);
+
+            Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine($"Total number of rolls = {numRolls}.");
+
+            // In the first class, use that array to print a histogram (using the * character)
+            // that shows the total percentage each number was rolled. Each * will represent
+            // 1% of the total rolls.
+            for (int total = 2; total <= 12; total++)
+            {
+                Console.Write($"{total}: ");
+                int percentage = (int)Math.Round((double)(results[total] * 100) / numRolls);
+
+                for (int percent = 0; percent < percentage; percent++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
+
